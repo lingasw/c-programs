5f2cec52c16f7839f4801063ae0003ca3fd3aeb5
LINGASWAMY <54500358+LINGASWAMY2509@users.noreply.github.com>
2020-07-04 12:43:09 +0530

    Update array_out_Put_999.c

--- array_out_Put_999.c
+++ array_out_Put_999.c
@@ -2,17 +2,18 @@
 
 int main()
 { 
-    int i=0,j,count=0;
+    int i=0,j,count=0,f=0;
     int b[3];
     int a[6]={5,6,3,7,2,4};
     for(i=0;i<6;i++)
     {
-        for(j=0;j<6;i++)
+        for(j=i;j<6;j++)
         {
            if((a[i]+a[j])==9)
            {
             printf("the two numbers are = %d,%d",a[i],a[j]);
-            b[i]=a[i]+a[j];
+            b[f]=a[i]+a[j];
+               f++;
             count+=1;
            }
         }
